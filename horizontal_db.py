db874c022c3ff7767457715ef025a6d4f2e59b61
bruno taborda <bruno.taborda1711@gmail.com>
2024-02-13 10:30:11 -0300

    finalizado horizontal

--- horizontal_db.py
+++ horizontal_db.py
@@ -1,19 +1,42 @@
 from psycopg2 import sql
 
-def select_query(dicionario):
+def select_query(dictionary):
     query = sql.SQL('SELECT * FROM public.retro_horizontal WHERE "AUDITORIA" = %s AND "RODOVIA" = %s AND "KM"=%s AND "METROS"=%s AND "DATA"= %s' )
-    values = (dicionario['AUDITORIA'], dicionario['RODOVIA'], str(dicionario['KM']), str(dicionario['METROS']), str(dicionario['DATA']))
+    values = (dictionary['AUDITORIA'], dictionary['RODOVIA'], str(dictionary['KM']), str(dictionary['METROS']), str(dictionary['DATA']))
     return query, values
 
-def insert_query(dicionario):
-    colunas = ', '.join([f'"{coluna}"' for coluna in dicionario.keys() if coluna != 'ID'])
-    placeholders = ', '.join(['%s' for coluna in range(len(dicionario.keys())) if coluna != 'ID'])
-    
-    insert_query = f"INSERT INTO public.retro_horizontal ({colunas}) VALUES ({placeholders}) RETURNING chave;"
-    return insert_query, tuple([valor for coluna, valor in dicionario.items() if coluna != 'ID'])
+def insert_query(dictionary):
+    integer_columns = ["AUDITORIA", "KM_I", "METRO_I", "ID_USUARIO"]
+    columns = ', '.join([f'"{column}"' for column in dictionary.keys() if column != 'ID'])
+    placeholders = ', '.join(['%s' for _ in range(len(dictionary)-1)])
+    values = []
 
-def update_query(dicionario):
-    set_clause = ', '.join([f'"{coluna}" = %s' for coluna in dicionario.keys() if coluna != 'ID'])
-    
-    update_query = f"UPDATE public.retro_horizontal SET {set_clause} WHERE \"ID\" = %s;"
-    return update_query, tuple([valor for coluna, valor in dicionario.items()] + [dicionario['ID']])
+    for column, value in list(dictionary.items())[1:]: 
+        if column in integer_columns and value is not None:
+            values.append(int(value))
+        else:
+            values.append('' if value is None else value)
+
+    values = ['' if value is None else value for value in values]
+    insert_query = f"INSERT INTO public.retro_horizontal ({columns}) VALUES ({placeholders});" 
+
+    return insert_query, tuple(values)
+
+def update_query(dictionary, result):
+    integer_columns = ["AUDITORIA", "KM_I", "METRO_I", "ID_USUARIO"]
+    set_clause = ', '.join([f'"{column}" = %s' for column in dictionary.keys() if column != 'ID'])
+    values = []
+
+    for column, value in list(dictionary.items())[1:]:
+        if column in integer_columns and value is not None:
+            values.append(int(value))
+        else:
+            values.append('' if value is None else value)
+
+    where_condition = '"ID"=%s'   
+    update_query = f'UPDATE public.retro_horizontal SET {set_clause} WHERE {where_condition}' 
+    where_values = [result[0]]
+
+    return update_query, tuple(values + where_values)
+
+
