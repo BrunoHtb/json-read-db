df6da2b2d938526e097ad457d1e84c16ce841cb6
Bruno Taborda <bruno.taborda1711@gmail.com>
2024-03-06 10:20:21 -0300

    Fazendo Update apenas em elementos antes do dia 24/01/2024

--- horizontal_db.py
+++ horizontal_db.py
@@ -1,8 +1,8 @@
 from psycopg2 import sql
 
 def select_query(dictionary):
-    query = sql.SQL('SELECT * FROM public.retro_horizontal WHERE "AUDITORIA" = %s AND "RODOVIA" = %s AND "KM_I"=%s AND "METRO_I"=%s AND "DATA"= %s AND "SENTIDO"=%s' )
-    values = (dictionary['AUDITORIA'], dictionary['RODOVIA'], str(dictionary['KM_I']), str(dictionary['METRO_I']), str(dictionary['DATA']), str(dictionary['SENTIDO']))
+    query = sql.SQL('SELECT *, "DATA_ALTERACAO" FROM public.retro_horizontal WHERE "AUDITORIA" = %s AND "RODOVIA" = %s AND "KM_I"=%s AND "METRO_I"=%s AND "DATA"= %s AND "SENTIDO"=%s AND "VIA"=%s' )
+    values = (dictionary['AUDITORIA'], dictionary['RODOVIA'], str(dictionary['KM_I']), str(dictionary['METRO_I']), str(dictionary['DATA']), str(dictionary['SENTIDO']), str(dictionary['VIA']))
     return query, values
 
 def insert_query(dictionary):
@@ -10,6 +10,9 @@ def insert_query(dictionary):
     columns = ', '.join([f'"{column}"' for column in dictionary.keys() if column != 'ID'])
     placeholders = ', '.join(['%s' for _ in range(len(dictionary)-1)])
     values = []
+
+    dictionary["KM"] = set_variable_lenght(dictionary["KM"])
+    dictionary["METRO"] = set_variable_lenght(dictionary["METRO"])
 
     for column, value in list(dictionary.items())[1:]: 
         if column in integer_columns and value is not None:
@@ -25,6 +28,8 @@ def insert_query(dictionary):
 def update_query(dictionary, result):
     integer_columns = ["AUDITORIA", "KM_I", "METRO_I", "ID_USUARIO"]
     set_clause = ', '.join([f'"{column}" = %s' for column in dictionary.keys() if column != 'ID'])
+    dictionary["KM"] = set_variable_lenght(dictionary["KM"])
+    dictionary["METRO"] = set_variable_lenght(dictionary["METRO"])
     values = []
 
     for column, value in list(dictionary.items())[1:]:
@@ -40,3 +45,10 @@ def update_query(dictionary, result):
     return update_query, tuple(values + where_values)
 
 
+def set_variable_lenght(variable):
+    if len(variable) == 2:
+        return "0" + variable
+    elif len(variable) == 1:
+        return "00" + variable
+
+    return variable
--- vertical_db.py
+++ vertical_db.py
@@ -1,16 +1,19 @@
 from psycopg2 import sql
 
 def select_query(dictionary):
-    query = sql.SQL('SELECT * FROM public.retro_vertical WHERE "AUDITORIA" = %s AND "RODOVIA" = %s AND "KM_I"=%s AND "METRO_I"=%s AND "DATA"= %s AND "SENTIDO"=%s' )
-    values = (dictionary['AUDITORIA'], dictionary['RODOVIA'], str(dictionary['KM_I']), str(dictionary['METRO_I']), str(dictionary['DATA']), str(dictionary['SENTIDO']))
+    query = sql.SQL('SELECT *, "DATA_ALTERACAO" FROM public.retro_vertical WHERE "AUDITORIA" = %s AND "RODOVIA" = %s AND "KM_I"=%s AND "METRO_I"=%s AND "DATA"= %s AND "SENTIDO"=%s AND "LADO_PISTA"=%s AND "VIA"=%s')
+    values = (dictionary['AUDITORIA'], dictionary['RODOVIA'], str(dictionary['KM_I']), str(dictionary['METRO_I']), str(dictionary['DATA']), str(dictionary['SENTIDO']), str(dictionary['LADO_PISTA']), str(dictionary['VIA']))
     return query, values
+
 
 def insert_query(dictionary):
     integer_columns = ["AUDITORIA", "KM_I", "METRO_I", "ID_USUARIO"]
     columns = ', '.join([f'"{column}"' for column in dictionary.keys() if column != 'ID'])
     placeholders = ', '.join(['%s' for _ in range(len(dictionary)-1)])
+    dictionary["KM"] = set_variable_lenght(str(dictionary["KM"]))
+    dictionary["METROS"] = set_variable_lenght(str(dictionary["METROS"]))
     values = []
-
+ 
     for column, value in list(dictionary.items())[1:]: 
         if column in integer_columns and value is not None:
             values.append(int(value))
@@ -22,8 +25,11 @@ def insert_query(dictionary):
 
     return insert_query, tuple(values)
 
+
 def update_query(dictionary, result):
     integer_columns = ["AUDITORIA", "KM_I", "METRO_I", "ID_USUARIO"]
+    dictionary["KM"] = set_variable_lenght(str(dictionary["KM"]))
+    dictionary["METROS"] = set_variable_lenght(str(dictionary["METROS"]))
     set_clause = ', '.join([f'"{column}" = %s' for column in dictionary.keys() if column != 'ID'])
     values = []
 
@@ -38,3 +44,12 @@ def update_query(dictionary, result):
     where_values = [result[0]]
 
     return update_query, tuple(values + where_values)
+
+
+def set_variable_lenght(variable):
+    if len(variable) == 2:
+        return "0" + variable
+    elif len(variable) == 1:
+        return "00" + variable
+
+    return variable
